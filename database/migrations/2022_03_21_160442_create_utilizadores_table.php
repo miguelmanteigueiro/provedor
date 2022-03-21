cda827e6b5b5c2fef1f301724062e2b39c87b561
Miguel Manteigueiro <miguel.manteigueiro@ubi.pt>
2022-03-21 16:39:42 +0000

    Remove default migrations and add sample ones

--- database/migrations/2022_03_21_160442_create_utilizadores_table.php
+++ database/migrations/2022_03_21_160442_create_utilizadores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUtilizadoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+        Schema::create('utilizadores', function (Blueprint $table) {
+            $table->id('utilizador_id');
+            $table->string('nome');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('senha');
         });
     }
 
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('utilizadores');
     }
 }
